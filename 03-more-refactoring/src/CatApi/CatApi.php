9dc2c5de38e1a4878813a2e0b863973bd1a8c5cd
Pedro Morales <pedromorales@grupojuinsa.es>
2020-01-23 11:54:34 +0100

    creo funcion para checkear url de fichero

--- 03-more-refactoring/src/CatApi/CatApi.php
+++ 03-more-refactoring/src/CatApi/CatApi.php
@@ -4,28 +4,36 @@ namespace CatApi;
 
 class CatApi
 {
+    protected $image_dir = "/../../cache/random";
+
     public function getRandomImage()
     {
-        if (!file_exists(__DIR__ . '/../../cache/random')
-            || time() - filemtime(__DIR__ . '/../../cache/random') > 3) {
-            $responseXml = @file_get_contents(
-                'http://thecatapi.com/api/images/get?format=xml&type=jpg'
-            );
-            if (!$responseXml) {
-                // the cat API is down or something
-                return 'http://cdn.my-cool-website.com/default.jpg';
-            }
+        if (!file_exists(__DIR__ . $this->image_dir) || time() - filemtime(__DIR__ . $this->image_dir) > 3) {
+
+            $responseXml = $this->checkFileExists();
 
             $responseElement = new \SimpleXMLElement($responseXml);
 
             file_put_contents(
-                __DIR__ . '/../../cache/random',
+                __DIR__ . $this->image_dir,
                 (string)$responseElement->data->images[0]->image->url
             );
 
             return (string)$responseElement->data->images[0]->image->url;
         } else {
-            return file_get_contents(__DIR__ . '/../../cache/random');
+            return file_get_contents(__DIR__ . $this->image_dir);
         }
+    }
+
+    function checkFileExists()
+    {
+        $responseXml = @file_get_contents('http://thecatapi.com/api/images/get?format=xml&type=jpg');
+
+        if (!$responseXml) {
+            // the cat API is down or something
+            return 'http://cdn.my-cool-website.com/default.jpg';
+        }
+
+        return $responseXml;
     }
 }
