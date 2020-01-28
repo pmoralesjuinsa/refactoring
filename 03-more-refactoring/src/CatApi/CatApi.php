e75f0e47745b89b988300ad45546bc8746545085
Pedro Morales <pedromorales@grupojuinsa.es>
2020-01-28 13:13:55 +0100

    elimino el método checkfile por otro que obtenga el xml necesario y que genera excepción por si es necesario recogerlo en los métodos del run.php

--- 03-more-refactoring/src/CatApi/CatApi.php
+++ 03-more-refactoring/src/CatApi/CatApi.php
@@ -3,6 +3,7 @@
 namespace CatApi;
 
 use Core\Tools;
+use MongoDB\Driver\Exception\Exception;
 
 class CatApi
 {
@@ -13,9 +14,7 @@ class CatApi
     {
         if (!file_exists(__DIR__ . Tools::$image_dir) || time() - filemtime(__DIR__ . Tools::$image_dir) > $this->created_X_seconds_ago) {
 
-            $responseXml = $this->checkFileExists();
-
-            $responseElement = new \SimpleXMLElement($responseXml);
+            $responseElement = $this->getCatXML();
 
             file_put_contents(
                 __DIR__ . Tools::$image_dir,
@@ -28,40 +27,35 @@ class CatApi
         }
     }
 
-    public function checkFileExists()
+    public function getCatXML($id='')
     {
-        $responseXml = $this->getCatXML();
 
-        if (!$responseXml) {
-            // the cat API is down or something
+        if($id != '') {
+            $responseXml = @file_get_contents($this->xml_random_cat_url."&image_id=".$id);
+        } else {
+            $responseXml = @file_get_contents($this->xml_random_cat_url);
+        }
+
+        try {
+            $responseElement = new \SimpleXMLElement($responseXml);
+        } catch (Exception $exception) {
             return 'http://cdn.my-cool-website.com/default.jpg';
         }
 
-        return $responseXml;
-    }
-
-    public function getCatXML($id='')
-    {
-        if($id != '') {
-            return @file_get_contents($this->xml_random_cat_url."&image_id=".$id);
-        }
-
-        return @file_get_contents($this->xml_random_cat_url);
+        return $responseElement;
     }
 
     public function getCatGifUrl($id)
     {
         $responseXml = $this->getCatXML($id);
-        $responseElement = new \SimpleXMLElement($responseXml);
 
-        return $responseElement->data->images[0]->image->url;
+        return $responseXml->data->images[0]->image->url;
     }
 
     public function getRandomCatGifUrl()
     {
         $responseXml = $this->getCatXML();
-        $responseElement = new \SimpleXMLElement($responseXml);
 
-        return $responseElement->data->images[0]->image->url;
+        return $responseXml->data->images[0]->image->url;
     }
 }
